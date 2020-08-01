9a68f29e7b58474f850b7cc9a5c7d5b2b833a5a5
Ahmet Avcı <ahmetavci@albil.com.tr>
2020-08-01 18:50:57 +0300

    master - firebase datasının bir kısmı ilgili componentlere bağlandı

--- Pages/QuestionDetailPage.js
+++ Pages/QuestionDetailPage.js
@@ -1,26 +1,41 @@
 import React, { Component } from "react";
-import { Container, Content, Card, CardItem, Text, Body, H3 } from "native-base";
-import { InitializeFireBaseDb } from '../Firebase/Firebase';
+import { StyleSheet } from 'react-native';
+import { Container, Content, Card, CardItem, Text, Body, H3, Button } from "native-base";
 import { Row, Grid } from "react-native-easy-grid";
 
 export class QuestionDetailPage extends Component {
+    state = {
+        totalTrue: 0,
+        totalFalse: 0
+    }
+
+    // TODO : TEST KATEGORİSİ ALTINDAKİ BİR TESTİN HEMEN ALTINDA TEK BİR SORU VE CEVAPLARI VAR. O KISMIN ARRAY ŞEKLİNDE GELMESİ GEREKİYOR.
+
+    chooseAnswer(isCorrect) {
+        console.log(isCorrect);
+    }
 
     render() {
-        const firebaseDb = InitializeFireBaseDb();
-        firebaseDb.database().ref("/").on('value', (snapshot => { console.log(snapshot.val()) }));
         return (
             <Container>
                 <Grid>
                     <Row>
                         <Content>
-                            <H3>{this.props.route.params.title}</H3>
+                            <H3>{this.props.route.params.header}</H3>
                             <Card>
                                 <CardItem>
                                     <Body>
-                                        <Text>Soru İçeriği</Text>
+                                        <Text>{this.props.route.params.data.Question}</Text>
                                     </Body>
                                 </CardItem>
                             </Card>
+                            {
+                                Object.keys(this.props.route.params.data.Answers).map((key, index) =>
+                                    <Button key={key} small full light style={styles.margin} onPress={() => this.chooseAnswer(Object.values(this.props.route.params.data.Answers)[index][1])}>
+                                        <Text>{key} - {Object.values(this.props.route.params.data.Answers)[index]}</Text>
+                                    </Button>
+                                )
+                            }
                         </Content>
                     </Row>
                 </Grid>
@@ -29,10 +44,10 @@ export class QuestionDetailPage extends Component {
     }
 }
 
-// const styles = StyleSheet.create({
-//   lineHeight: {
-//     lineHeight: 22
-//   }
-// });
+const styles = StyleSheet.create({
+    margin: {
+        marginBottom: 5
+    }
+});
 
 export default QuestionDetailPage;
--- Pages/QuestionListPage.js
+++ Pages/QuestionListPage.js
@@ -1,165 +1,123 @@
 import React, { Component } from "react";
 import { Container, List, ListItem, Text, Content, Left, Right, Icon } from "native-base";
-import { QuestionDetailPage } from "../Pages/QuestionDetailPage"
 import { Row, Grid } from "react-native-easy-grid";
-import 'react-native-gesture-handler'; 
-import { HeaderComp, FooterComp } from "../Components/index";
+import 'react-native-gesture-handler';
+import { InitializeFireBaseDb } from '../Firebase/Firebase';
 
 export class QuestionListPage extends Component {
-  render() {
-    return (
-      <Container>
-        <Grid>
-          <Row>
-            <Content>
-              <List>
-                <ListItem itemDivider>
-                  <Text>Giriş Seviyesi Testler</Text>
-                </ListItem>
-                <ListItem onPress={() => this.props.navigation.navigate('Detail', {name: 'Test1', title: 'Giriş Seviye Test 1'})}>
-                  <Left>
-                    <Text>Giriş 1</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem onPress={() => this.props.navigation.navigate('Detail', {name: 'Test2', title: 'Giriş Seviye Test 2'})}>
-                  <Left>
-                    <Text>Giriş 2</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Giriş 3</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Giriş 4</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem itemDivider>
-                  <Text>Orta Seviye Testler</Text>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Orta Seviye 1</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Orta Seviye 2</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Orta Seviye 3</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Orta Seviye 4</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem itemDivider>
-                  <Text>Zor Seviye Testler</Text>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Zor Seviye 1</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Zor Seviye 2</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Zor Seviye 3</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Zor Seviye 4</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem itemDivider>
-                  <Text>Derslere Özel Testler(30 SORU)</Text>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Coğrafya Testi(30 SORU)</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Türkçe Testi(30 SORU)</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Genel Kültür Testi(30 SORU)</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Left>
-                    <Text>Matematik Testi(30 SORU)</Text>
-                  </Left>
-                  <Right>
-                    <Icon name="arrow-forward" />
-                  </Right>
-                </ListItem>
-              </List>
-            </Content>
-          </Row>
-        </Grid>
-      </Container>
-    );
-  }
+    state = {
+        allQuestions: null
+    }
+
+    componentDidMount() {
+        if (!this.state.allQuestions) {
+            const firebaseDb = InitializeFireBaseDb();
+            firebaseDb.database().ref("KpssTests").on('value', (snapshot => {
+                if (snapshot.val()) {
+                    this.setState({ allQuestions: snapshot.val() });
+                }
+            }));
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+                <Grid>
+                    <Row>
+                        <Content>
+                            <List>
+                                <ListItem itemDivider>
+                                    <Text>Giriş Seviye Testler</Text>
+                                </ListItem>
+                                {
+                                    this.state.allQuestions ?
+                                        Object.keys(this.state.allQuestions.Basic).map((x, i) =>
+                                            <ListItem onPress={() => this.props.navigation.navigate('Detail', { data: Object.values(this.state.allQuestions.Basic)[i], header: x })}>
+                                                <Left>
+                                                    <Text>{x}</Text>
+                                                </Left>
+                                                <Right>
+                                                    <Icon name="arrow-forward" />
+                                                </Right>
+                                            </ListItem>
+                                        ) :
+                                        <ListItem>
+                                            <Left>
+                                                <Text>...</Text>
+                                            </Left>
+                                        </ListItem>
+                                }
+                                <ListItem itemDivider>
+                                    <Text>Orta Seviye Testler</Text>
+                                </ListItem>
+                                {
+                                    this.state.allQuestions ?
+                                        Object.keys(this.state.allQuestions.Middle).map((x, i) =>
+                                            <ListItem onPress={() => this.props.navigation.navigate('Detail', { data: Object.values(this.state.allQuestions.Basic)[i], header: x })}>
+                                                <Left>
+                                                    <Text>{x}</Text>
+                                                </Left>
+                                                <Right>
+                                                    <Icon name="arrow-forward" />
+                                                </Right>
+                                            </ListItem>
+                                        ) :
+                                        <ListItem>
+                                            <Left>
+                                                <Text>...</Text>
+                                            </Left>
+                                        </ListItem>
+                                }
+                                <ListItem itemDivider>
+                                    <Text>Zor Seviye Testler</Text>
+                                </ListItem>
+                                {
+                                    this.state.allQuestions ?
+                                        Object.keys(this.state.allQuestions.Hard).map((x, i) =>
+                                            <ListItem onPress={() => this.props.navigation.navigate('Detail', { data: Object.values(this.state.allQuestions.Basic)[i], header: x })}>
+                                                <Left>
+                                                    <Text>{x}</Text>
+                                                </Left>
+                                                <Right>
+                                                    <Icon name="arrow-forward" />
+                                                </Right>
+                                            </ListItem>
+                                        ) :
+                                        <ListItem>
+                                            <Left>
+                                                <Text>...</Text>
+                                            </Left>
+                                        </ListItem>
+                                }
+                                <ListItem itemDivider>
+                                    <Text>Derslere Özel Testler(30 SORU)</Text>
+                                </ListItem>
+                                {
+                                    this.state.allQuestions ?
+                                        Object.keys(this.state.allQuestions.Other).map((x, i) =>
+                                            <ListItem onPress={() => this.props.navigation.navigate('Detail', { data: Object.values(this.state.allQuestions.Basic)[i], header: x })}>
+                                                <Left>
+                                                    <Text>{x}</Text>
+                                                </Left>
+                                                <Right>
+                                                    <Icon name="arrow-forward" />
+                                                </Right>
+                                            </ListItem>
+                                        ) :
+                                        <ListItem>
+                                            <Left>
+                                                <Text>...</Text>
+                                            </Left>
+                                        </ListItem>
+                                }
+                            </List>
+                        </Content>
+                    </Row>
+                </Grid>
+            </Container>
+        );
+    }
 }
 
 export default QuestionListPage;
